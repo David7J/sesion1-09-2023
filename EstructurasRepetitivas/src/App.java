cd23c192c9ac9e5efc71a6049be82d1cb980bef3
David7J <142626297+David7J@users.noreply.github.com>
2023-09-01 14:17:00 -0600

    Add files via upload

--- EstructurasRepetitivas/src/App.java
+++ EstructurasRepetitivas/src/App.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner lector = new Scanner(System.in);
+
+        System.out.println("Sentencias repetitivas");
+        System.out.println("Sentencia while");
+        byte n = lector.nextByte();
+        byte x = 1;
+        
+        // No permitir valores menores o iguales a cero
+
+        while(n <= 0){
+        System.out.println("Que tabla de multiplicacion desea mostrar? ");    
+        n = lector.nextByte();
+        }
+
+        while(x <= 10){
+            System.out.println(n + "x" + x + "=" + n*x);
+            x++; 
+        }
+        System.out.println("Despues del while");
+        System.out.println("Fin");
+
+        lector.close();
     }
 }
